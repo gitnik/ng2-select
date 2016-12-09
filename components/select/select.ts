7b7a1b432dc0b490f5541966a91aed4a92e92466
Nik Karbaum <nik.karbaum@gmail.com>
2016-12-09 11:56:41 +0100

    fix condition for backspacing

--- components/select/select.ts
+++ components/select/select.ts
@@ -334,7 +334,7 @@ export class SelectComponent implements OnInit {
     if (!isUpMode && e.keyCode === 8) {
       let el:any = this.element.nativeElement
         .querySelector('div.ui-select-container > input');
-      if (!el.value || el.value.length <= 0) {
+      if (!el.value || el.value.length > 0) {
         if (this.active.length > 0) {
           this.remove(this.active[this.active.length - 1]);
         }
